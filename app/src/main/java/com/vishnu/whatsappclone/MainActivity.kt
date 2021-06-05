01eb3ac98350a3a09148c3618177788b95cb5cf2
Vishnu Anand <vishnuanand2000@gmail.com>
2021-06-05 23:40:41 +0530

    Imported Intent library

--- app/src/main/java/com/vishnu/whatsappclone/MainActivity.kt
+++ app/src/main/java/com/vishnu/whatsappclone/MainActivity.kt
@@ -1,13 +1,22 @@
 package com.vishnu.whatsappclone
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        button_Test()
+    }
 
+    private fun button_Test() {
+        loginButton.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }

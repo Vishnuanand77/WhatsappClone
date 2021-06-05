c67fa659724a225634516955c2374daa1180c0bb
Vishnu Anand <vishnuanand2000@gmail.com>
2021-06-06 03:03:59 +0530

    Added intent to a new activity

--- app/src/main/java/com/vishnu/whatsappclone/CreateAccountActivity.kt
+++ app/src/main/java/com/vishnu/whatsappclone/CreateAccountActivity.kt
@@ -1,20 +1,15 @@
 package com.vishnu.whatsappclone
 
-import android.content.ContentValues.TAG
-import android.nfc.Tag
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
 import android.util.Log
 import android.widget.Toast
 import com.google.android.gms.tasks.Task
-import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ktx.database
-import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_create_account.*
 
 class CreateAccountActivity : AppCompatActivity() {
@@ -35,7 +30,7 @@ class CreateAccountActivity : AppCompatActivity() {
             //Getting User Input
             val username = CA_usernameEditText.text.toString()
             val email = CA_SignUpemailIdEditText.text.toString().trim()
-            val password = CA_passwordEditText.text.toString().trim()
+            val password = loginScreenPasswordEditText.text.toString().trim()
 
             //Validating null input. Move to authentication only if text input is not empty
             if (!TextUtils.isEmpty(username) || !TextUtils.isEmpty(email) || !TextUtils.isEmpty(password)) {
@@ -71,7 +66,16 @@ class CreateAccountActivity : AppCompatActivity() {
                         task: Task<Void> ->
                             if (task.isSuccessful) {
                                 Log.d("Firebase Database: success ", "DB entry successful")
-                                Toast.makeText(this, "User Created", Toast.LENGTH_SHORT).show()
+                                //Toast.makeText(this, "User Created", Toast.LENGTH_SHORT).show()
+
+                                //Starting a new activity on success
+                                val dashboardIntent = Intent(this, DashboardActivity::class.java)
+                                //Extras
+                                dashboardIntent.putExtra("username", username)
+
+                                //Starting the activity
+                                startActivity(dashboardIntent)
+                                finish() //Ending the current activity
                             } else {
                                 Log.d("Firebase Database: failure ", task.exception.toString())
                                 Toast.makeText(this, "Database failed", Toast.LENGTH_SHORT).show()

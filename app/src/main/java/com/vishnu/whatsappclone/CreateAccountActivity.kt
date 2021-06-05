ff69a7af55e22929a75ebe6ac4c223b77cc30b4a
Vishnu Anand <vishnuanand2000@gmail.com>
2021-06-06 00:06:30 +0530

    Initialized Firebase Instance and created createAccount function template

--- app/src/main/java/com/vishnu/whatsappclone/CreateAccountActivity.kt
+++ app/src/main/java/com/vishnu/whatsappclone/CreateAccountActivity.kt
@@ -2,10 +2,25 @@ package com.vishnu.whatsappclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
 
 class CreateAccountActivity : AppCompatActivity() {
+
+    //Variable Instantiations
+    var mAuth: FirebaseAuth? = null
+    var mDatabase: DatabaseReference? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_account)
+
+        //Getting firebase instance
+        mAuth = FirebaseAuth.getInstance()
+    }
+
+    //Function to create a new account
+    fun createAccount(email: String, password: String, displayName: String) {
+        
     }
 }

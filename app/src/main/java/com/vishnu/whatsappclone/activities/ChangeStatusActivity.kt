645d40e6a0320629e42655ebff0dd7744f97f0a9
Vishnu Anand <vishnuanand2000@gmail.com>
2021-06-07 00:28:51 +0530

    Implemented updating status in firebase database

--- app/src/main/java/com/vishnu/whatsappclone/activities/ChangeStatusActivity.kt
+++ app/src/main/java/com/vishnu/whatsappclone/activities/ChangeStatusActivity.kt
@@ -5,10 +5,19 @@ import android.os.Bundle
 import android.text.TextUtils
 import android.util.Log
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import com.vishnu.whatsappclone.R
 import kotlinx.android.synthetic.main.activity_change_status.*
 
 class ChangeStatusActivity : AppCompatActivity() {
+
+    //Firebase Variables
+    var mDatabase: DatabaseReference? = null
+    var mCurrentUser: FirebaseUser? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_change_status)
@@ -26,11 +35,30 @@ class ChangeStatusActivity : AppCompatActivity() {
         changeStatusButton.setOnClickListener {
             //Text field authentication
             val newStatus = changeStatus_EditText.text.toString()
-            if (newStatus != "") {
-                changeStatus_StatusDisplay.text = newStatus
+            if (!TextUtils.isEmpty(newStatus)) {
+                //UI change
+                changeStatus_StatusDisplay.text = newStatus.trim()
+
+                //Adding to database
+                mCurrentUser = FirebaseAuth.getInstance().currentUser
+                val userId = mCurrentUser!!.uid
+                mDatabase = FirebaseDatabase.getInstance().reference.child("Users").child(userId)
+
+                mDatabase!!.child("status").setValue(newStatus).addOnCompleteListener {
+                    task -> if (task.isSuccessful) {
+                    Toast.makeText(this, "Status Updated!", Toast.LENGTH_SHORT).show()
+                    changeStatus_EditText.text.clear()
+                    changeStatus_EditText.clearFocus()
+                    } else {
+                        Log.d("Change Status Activity Error ", task.exception.toString())
+                        Toast.makeText(this, "An error occurred!", Toast.LENGTH_SHORT).show()
+                    }
+                }
+
             } else {
                 Toast.makeText(this, "Pick a new status", Toast.LENGTH_SHORT).show()
             }
+
         }
     }
 }

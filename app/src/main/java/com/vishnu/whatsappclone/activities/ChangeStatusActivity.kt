36aea0cdc486945f4fc729e151d43463e5553b8b
Vishnu Anand <vishnuanand2000@gmail.com>
2021-06-06 23:18:15 +0530

    Implemented changing the status

--- app/src/main/java/com/vishnu/whatsappclone/activities/ChangeStatusActivity.kt
+++ app/src/main/java/com/vishnu/whatsappclone/activities/ChangeStatusActivity.kt
@@ -2,6 +2,9 @@ package com.vishnu.whatsappclone.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
+import android.widget.Toast
 import com.vishnu.whatsappclone.R
 import kotlinx.android.synthetic.main.activity_change_status.*
 
@@ -13,8 +16,21 @@ class ChangeStatusActivity : AppCompatActivity() {
         supportActionBar!!.title = "Change Status"
 
         if (intent.extras != null) {
-            var currentStatus = intent.extras!!.get("status")
-            changeStatus_StatusDisplay.setText(currentStatus.toString())
+            val currentStatus = intent.extras!!.get("status")
+            changeStatus_StatusDisplay.text = currentStatus.toString()
+        }
+        if (intent.extras!!.equals(null)) {
+            changeStatus_StatusDisplay.setText(R.string.default_status_display)
+        }
+
+        changeStatusButton.setOnClickListener {
+            //Text field authentication
+            val newStatus = changeStatus_EditText.text.toString()
+            if (newStatus != "") {
+                changeStatus_StatusDisplay.text = newStatus
+            } else {
+                Toast.makeText(this, "Pick a new status", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }

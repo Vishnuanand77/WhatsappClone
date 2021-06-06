f5af3bfdd8256fcf3d74091a34d8cea071a1cfeb
Vishnu Anand <vishnuanand2000@gmail.com>
2021-06-06 22:23:02 +0530

    Getting data from firebase and displaying username and status

---
+++ app/src/main/java/com/vishnu/whatsappclone/activities/ChangeStatusActivity.kt
@@ -0,0 +1,11 @@
+package com.vishnu.whatsappclone.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChangeStatusActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_change_status)
+    }
+}
--- app/src/main/java/com/vishnu/whatsappclone/activities/SettingsActivity.kt
+++ app/src/main/java/com/vishnu/whatsappclone/activities/SettingsActivity.kt
@@ -2,11 +2,49 @@ package com.vishnu.whatsappclone.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.*
+import com.google.firebase.storage.StorageReference
 import com.vishnu.whatsappclone.R
+import kotlinx.android.synthetic.main.activity_settings.*
 
 class SettingsActivity : AppCompatActivity() {
+
+    //Firebase variables
+    var mDatabase: DatabaseReference? = null
+    var mCurrentUser: FirebaseUser? =null
+    var mStorageReference: StorageReference? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        //Instantiating Firebase variables
+        mCurrentUser = FirebaseAuth.getInstance().currentUser
+        val userId = mCurrentUser!!.uid
+
+        mDatabase = FirebaseDatabase.getInstance().reference.child("Users").child(userId)
+        mDatabase!!.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                //Gets a snapshot of the entries against the userId
+                var username = dataSnapshot.child("username").value
+                var image = dataSnapshot.child("image").value
+                var status = dataSnapshot.child("status").value
+                var userThumbnail = dataSnapshot.child("thumbnail").value
+
+                //Setting the UI to what we retrieved from the database
+                settings_usernameDisplay.text = username.toString()
+                settings_StatusDisplay.text = status.toString()
+            }
+            override fun onCancelled(databaseError: DatabaseError) {
+                //Records errors with respect to database
+                Log.d("Firebase Database Error", databaseError.toString())
+            }
+        })
+
+        //OnClick Listener
+
     }
 }
